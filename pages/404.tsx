ec498710aa76f2bdf77b0074f81e207f7e377a0b
Rein Oterholm <rein@otter.productions>
2021-07-10 23:56:22 +0200

    Fix 404 styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ import Container from '../components/container';
 export default function FourOhFour() {
   return (
     <Container>
-      <h1 className="mt-16">404 - Page Not Found</h1>
+      <h1 className="mt-16 text-center">404 - Page Not Found</h1>
       <Link href="/">
         <a>Go back home</a>
       </Link>
